bf9a6b62e979a7b88d110807c6a58399a65ebfe8
Dima <dmytrotus@gmail.com>
2020-12-11 20:07:05 +0100

    protect api routes

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 //Auth::routes();
-Route::get('/', 'HomeController@tasksAll');
-/*Route::get('/', function(){
+Route::get('/', function(){
 	return redirect('/user/projects');
-});*/
+});
 
 Route::get('/register', 'Auth\RegisterController@showRegistrationForm');
 Route::post('/register', 'Auth\RegisterController@register')->name('register');
